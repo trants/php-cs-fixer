8eea497ce12325622080a74a07ee9c02e46bdf66
Son Tran Thanh <286.trants@gmail.com>
2024-04-18 16:24:17 +0700

    Add copyright information

--- src/index.php
+++ src/index.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * *************************************************************************
+ * Copyright (c) VSP Co., Ltd - All Rights Reserved
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ */
 
 namespace Vspc\PhpCsFixer;
 
